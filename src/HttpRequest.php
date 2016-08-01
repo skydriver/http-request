6b47541e49f607cf00e94c3fba0c63e07b747662
Damjan Krstevski <krstevski.damjan@live.com>
2016-08-01 12:44:04 +0200

    Add sources

---
+++ src/HttpRequest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace skydriver\HttpRequest;
+
+
+class HttpRequest extends Request
+{
+
+}
---
+++ src/Request.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace skydriver\HttpRequest;
+
+
+abstract class Request
+{
+
+}
